1f01af7263eba691a0072f041e1ad40407977228
seoky99 <seokyoung99@gmail.com>
2024-09-19 17:41:46 -0400

    minimal styling, changed logic per button click

--- logic.js
+++ logic.js
@@ -1,6 +1,15 @@
 let humanScore = 0;
 let computerScore = 0;
 
+//Capturing the documents' elements 
+const parent = document.querySelector(".user-choice");
+const temp = document.querySelector(".tempname");
+const playerWins = document.querySelector(".human-score");
+const aiWins = document.querySelector(".ai-score");
+
+parent.addEventListener("click", 
+    (e) => playRound(e.target.name, getComputerChoice()));
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     
@@ -22,50 +31,65 @@ function getHumanChoice() {
 
 function lostMessage(p, c) {
     computerScore++;
+    aiWins.textContent = computerScore; 
     return `You lost! ${p} loses to ${c}!`; 
 }
 
 function wonMessage(p, c) {
     humanScore++; 
+    playerWins.textContent = humanScore;
     return `You won! ${p} wins against ${c}!`;
+}
+
+function playRound(humanChoice, computerChoice) {
+    let decapitalized = humanChoice.toLowerCase().trim();
+
+    console.log(decapitalized, computerChoice);
+
+    let result = "";
+
+    if (decapitalized===computerChoice) {
+        result = `You tied! ${decapitalized} ties with ${computerChoice}!`;
+    } else {
+
+        switch(decapitalized) {
+            case "rock":
+                result = computerChoice==="paper" ? lostMessage(decapitalized, computerChoice) : wonMessage(decapitalized, computerChoice);
+                break;
+            case "scissors":
+                result = computerChoice==="rock" ? lostMessage(decapitalized, computerChoice) : wonMessage(decapitalized, computerChoice);
+                break;
+            case "paper":
+                result = computerChoice==="scissors" ? lostMessage(decapitalized, computerChoice) : wonMessage(decapitalized, computerChoice);
+                break;
+            default:
+                result = null; 
+        }
+    }
+
+    temp.textContent = result;  
+
+    if (humanScore == 5) {
+        alert("YOU ARE THE WINNER!");
+    }
+
+    if (computerScore == 5) {
+        alert("YOU ARE THE LOSER!");
+    }
+
+    return result; 
 }
 
 function playGame() {
 
-    function playRound(humanChoice, computerChoice) {
-        let decapitalized = humanChoice.toLowerCase().trim();
-    
-        console.log(decapitalized, computerChoice);
-    
-    
-        let result = "";
-    
-        if (decapitalized===computerChoice) {
-            result = `You tied! ${decapitalized} ties with ${computerChoice}!`;
-        } else {
-    
-            switch(decapitalized) {
-                case "rock":
-                    result = computerChoice==="paper" ? lostMessage(decapitalized, computerChoice) : wonMessage(decapitalized, computerChoice);
-                    break;
-                case "scissors":
-                    result = computerChoice==="rock" ? lostMessage(decapitalized, computerChoice) : wonMessage(decapitalized, computerChoice);
-                    break;
-                case "paper":
-                    result = computerChoice==="scissors" ? lostMessage(decapitalized, computerChoice) : wonMessage(decapitalized, computerChoice);
-                    break;
-                default:
-                    result = null; 
-            }
-        }
-        return result; 
-    }
-
+    /*
     for (let i=0; i<5; i++) {
         console.log(playRound(getHumanChoice(), getComputerChoice()));
         console.log(humanScore);
         console.log(computerScore);
     }
+
+    */
 }
 
 //playGame();
